b9da275857cc8d1d2e58fed3086e718e5019b5ac
Shamo <810755055@qq.com>
2024-04-06 17:30:20 +0800

    前台分类组件的封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@ import CustomNavbar from './components/CustomNavbar/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import type { BannerItem } from '@/types/home'
 import { ref } from 'vue'
+import CategoryPanel from './components/CategoryPanel/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerData = async () => {
@@ -20,8 +21,11 @@ onLoad(() => {
   <CustomNavbar /><!-- 自定义导航组件引入 -->
   <XtxSwiper :list="bannerList" /><!-- 自定义轮播图组件全局 -->
   <view class="index"></view>
+  <CategoryPanel />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
